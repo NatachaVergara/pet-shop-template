a267dba68de8db462aa3de4eda7e122edc2aeed5
Natacha Vergara <ntchvergara@gmail.com>
2022-11-30 11:19:55 -0300

    Header -  Footer

---
+++ src/Routes/AppRoutes.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from '../Views/Home'
+import NotFound from '../Views/NotFound'
+
+const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/home' element={<Home />} />
+
+
+
+
+            <Route path='*' element={<NotFound />} />
+        </Routes>
+    )
+}
+
+export default AppRoutes
